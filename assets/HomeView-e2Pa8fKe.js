d725dca5c6279ff425db6c1370eeb17210fc5ebe
xjm <1024892974@qq.com>
2025-07-09 10:55:08 +0800

    代码提交

---
+++ assets/HomeView-e2Pa8fKe.js
@@ -0,0 +1,82 @@
+import{m as w,c as _,o as p,a as u,b as r,w as o,E as P,d as z,e as c,f as B,g as E,r as R,h as D,i as C,t as S,j as y,k as F,l as I,n as L,p as M,u as x}from"./index-BNIpz0Sn.js";const N={viewBox:"0 0 1024 1024",width:"1.2em",height:"1.2em"};function O(d,n){return p(),_("svg",N,n[0]||(n[0]=[u("path",{fill:"currentColor",d:"m795.904 750.72l124.992 124.928a32 32 0 0 1-45.248 45.248L750.656 795.904a416 416 0 1 1 45.248-45.248zM480 832a352 352 0 1 0 0-704a352 352 0 0 0 0 704"},null,-1)]))}const k=w({name:"ep-search",render:O}),$=""+new URL("banner-CNZI6UaP.png",import.meta.url).href,j=""+new URL("logo-CHdJPfSZ.png",import.meta.url).href,b=(d,n)=>{const l=d.__vccOpts||d;for(const[e,t]of n)l[e]=t;return l},V={},H={class:"slider"};function G(d,n){const l=z,e=B,t=P;return p(),_("div",H,[n[18]||(n[18]=u("div",{class:"flex head"},[u("img",{class:"logo",src:j,alt:""}),u("span",null,"世界工厂")],-1)),r(t,{mode:"vertical",collapse:!0,class:"el-menu-slider"},{default:o(()=>[r(l,{index:"1"},{default:o(()=>n[0]||(n[0]=[c("服务端")])),_:1,__:[0]}),r(e,{index:"2"},{title:o(()=>n[1]||(n[1]=[c("需求方")])),default:o(()=>[r(l,{index:"2-1"},{default:o(()=>n[2]||(n[2]=[c("寻价找厂")])),_:1,__:[2]}),r(l,{index:"2-2"},{default:o(()=>n[3]||(n[3]=[c("以图找厂")])),_:1,__:[3]}),r(l,{index:"2-3"},{default:o(()=>n[4]||(n[4]=[c("图纸在线制作")])),_:1,__:[4]}),r(e,{index:"2-4"},{title:o(()=>n[5]||(n[5]=[c("供方")])),default:o(()=>[r(l,{index:"2-4-1"},{default:o(()=>n[6]||(n[6]=[c("找订单")])),_:1,__:[6]}),r(l,{index:"2-4-2"},{default:o(()=>n[7]||(n[7]=[c("外贸")])),_:1,__:[7]})]),_:1})]),_:1}),r(l,{index:"3"},{default:o(()=>n[8]||(n[8]=[c("商品")])),_:1,__:[8]}),r(e,{index:"4"},{title:o(()=>n[9]||(n[9]=[c("人力资源")])),default:o(()=>[r(l,{index:"4-4-1"},{default:o(()=>n[10]||(n[10]=[c("找工作")])),_:1,__:[10]}),r(l,{index:"4-4-2"},{default:o(()=>n[11]||(n[11]=[c("聘请员工")])),_:1,__:[11]})]),_:1}),r(e,{index:"5"},{title:o(()=>n[12]||(n[12]=[c("学院")])),default:o(()=>[r(l,{index:"5-4-1"},{default:o(()=>n[13]||(n[13]=[c("培训软件技能")])),_:1,__:[13]}),r(l,{index:"5-4-2"},{default:o(()=>n[14]||(n[14]=[c("培训机械技能")])),_:1,__:[14]})]),_:1}),r(e,{index:"6"},{title:o(()=>n[15]||(n[15]=[c("金融")])),default:o(()=>[r(l,{index:"6-4-1"},{default:o(()=>n[16]||(n[16]=[c("低息贷款")])),_:1,__:[16]}),r(l,{index:"6-4-2"},{default:o(()=>n[17]||(n[17]=[c("短期周转")])),_:1,__:[17]})]),_:1})]),_:1})])}const X=b(V,[["render",G],["__scopeId","data-v-f32e0d42"]]),W={viewBox:"0 0 1024 1024",width:"1.2em",height:"1.2em"};function Y(d,n){return p(),_("svg",W,n[0]||(n[0]=[u("path",{fill:"currentColor",d:"M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z"},null,-1)]))}const q=w({name:"ep-arrow-down",render:Y}),K={class:"flex topHeader"},Q={class:"flex el-dropdown-link"},Z={class:"lang"},J=E({__name:"TopHeader",setup(d){const n=R("中文");return(l,e)=>{const t=q,s=y,i=I,m=F,a=D,f=C;return p(),_("div",K,[r(a,null,{dropdown:o(()=>[r(m,null,{default:o(()=>[r(i,{onClick:e[0]||(e[0]=h=>n.value="中文")},{default:o(()=>e[3]||(e[3]=[c("中文")])),_:1,__:[3]}),r(i,{onClick:e[1]||(e[1]=h=>n.value="English")},{default:o(()=>e[4]||(e[4]=[c("English")])),_:1,__:[4]})]),_:1})]),default:o(()=>[u("span",Q,[e[2]||(e[2]=u("i",{class:"iconfont icon-earth"},null,-1)),u("span",Z,S(n.value),1),r(s,null,{default:o(()=>[r(t)]),_:1})])]),_:1}),r(f,{type:"",text:"",style:{"margin-left":"4px"}},{default:o(()=>e[5]||(e[5]=[c(" 登录 ")])),_:1,__:[5]}),r(f,{type:"primary",text:"",style:{"margin-left":"0"}},{default:o(()=>e[6]||(e[6]=[c(" 注册 ")])),_:1,__:[6]})])}}}),tt=b(J,[["__scopeId","data-v-085d3ec4"]]),et={class:"BgAnimation"},st=E({__name:"pointWave",setup(d){class n{constructor(e,t={}){t=Object.assign({antialias:!1,depthTest:!1,mousemove:!1,autosize:!0,side:"front",vertex:`
+        precision highp float;
+
+        attribute vec4 a_position;
+        attribute vec4 a_color;
+
+        uniform float u_time;
+        uniform vec2 u_resolution;
+        uniform vec2 u_mousemove;
+        uniform mat4 u_projection;
+
+        varying vec4 v_color;
+
+        void main() {
+
+          gl_Position = u_projection * a_position;
+          gl_PointSize = (10.0 / gl_Position.w) * 100.0;
+
+          v_color = a_color;
+
+        }`,fragment:`
+        precision highp float;
+
+        uniform sampler2D u_texture;
+        uniform int u_hasTexture;
+
+        varying vec4 v_color;
+
+        void main() {
+
+          if ( u_hasTexture == 1 ) {
+
+            gl_FragColor = v_color * texture2D(u_texture, gl_PointCoord);
+
+          } else {
+
+            gl_FragColor = v_color;
+
+          }
+
+        }`,uniforms:{},buffers:{},camera:{},texture:null,onUpdate:()=>{},onResize:()=>{}},t);const s=Object.assign({time:{type:"float",value:0},hasTexture:{type:"int",value:0},resolution:{type:"vec2",value:[0,0]},mousemove:{type:"vec2",value:[0,0]},projection:{type:"mat4",value:[1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]}},t.uniforms),i=Object.assign({position:{size:3,data:[]},color:{size:4,data:[]}},t.buffers),m=Object.assign({fov:60,near:1,far:1e4,aspect:1,z:100,perspective:!0},t.camera),a=document.createElement("canvas"),f=a.getContext("webgl",{antialias:t.antialias});if(!f)return!1;this.count=0,this.gl=f,this.canvas=a,this.camera=m,this.holder=e,this.onUpdate=t.onUpdate,this.onResize=t.onResize,this.data={},e.appendChild(a),this.createProgram(t.vertex,t.fragment),this.createBuffers(i),this.createUniforms(s),this.updateBuffers(),this.updateUniforms(),this.createTexture(t.texture),f.enable(f.BLEND),f.enable(f.CULL_FACE),f.blendFunc(f.SRC_ALPHA,f.ONE),f[t.depthTest?"enable":"disable"](f.DEPTH_TEST),t.autosize&&window.addEventListener("resize",h=>this.resize(h),!1),t.mousemove&&window.addEventListener("mousemove",h=>this.mousemove(h),!1),this.resize(),this.update=this.update.bind(this),this.time={start:performance.now(),old:performance.now()},this.update()}mousemove(e){const t=e.pageX/this.width*2-1,s=e.pageY/this.height*2-1;this.uniforms.mousemove=[t,s]}resize(e){const t=this.holder,s=this.canvas,i=this.gl,m=this.width=t.offsetWidth,a=this.height=t.offsetHeight,f=this.aspect=m/a,h=this.dpi=devicePixelRatio;s.width=m*h,s.height=a*h,s.style.width="100%",s.style.height="100%",i.viewport(0,0,m*h,a*h),i.clearColor(0,0,0,0),this.uniforms.resolution=[m,a],this.uniforms.projection=this.setProjection(f),this.onResize(m,a,h)}setProjection(e){const t=this.camera;if(t.perspective){t.aspect=e;const s=t.fov*(Math.PI/180),i=Math.tan(Math.PI*.5-.5*s),m=1/(t.near-t.far),a=[i/t.aspect,0,0,0,0,i,0,0,0,0,(t.near+t.far)*m,-1,0,0,t.near*t.far*m*2,0];return a[14]+=t.z,a[15]+=t.z,a}else return[2/this.width,0,0,0,0,-2/this.height,0,0,0,0,1,0,-1,1,0,1]}createShader(e,t){const s=this.gl,i=s.createShader(e);if(s.shaderSource(i,t),s.compileShader(i),s.getShaderParameter(i,s.COMPILE_STATUS))return i;console.log(s.getShaderInfoLog(i)),s.deleteShader(i)}createProgram(e,t){const s=this.gl,i=this.createShader(s.VERTEX_SHADER,e),m=this.createShader(s.FRAGMENT_SHADER,t),a=s.createProgram();s.attachShader(a,i),s.attachShader(a,m),s.linkProgram(a),s.getProgramParameter(a,s.LINK_STATUS)?(s.useProgram(a),this.program=a):(console.log(s.getProgramInfoLog(a)),s.deleteProgram(a))}createUniforms(e){const t=this.gl,s=this.data.uniforms=e;this.uniforms={},Object.keys(s).forEach(i=>{const m=s[i];m.location=t.getUniformLocation(this.program,"u_"+i)})}setUniform(e,t){const s=this.gl,i=this.data.uniforms[e];switch(i.value=t,i.type){case"int":{s.uniform1i(i.location,t);break}case"float":{s.uniform1f(i.location,t);break}case"vec2":{s.uniform2f(i.location,...t);break}case"vec3":{s.uniform3f(i.location,...t);break}case"vec4":{s.uniform4f(i.location,...t);break}case"mat2":{s.uniformMatrix2fv(i.location,!1,t);break}case"mat3":{s.uniformMatrix3fv(i.location,!1,t);break}case"mat4":{s.uniformMatrix4fv(i.location,!1,t);break}}}updateUniforms(){this.gl;const e=this.data.uniforms;Object.keys(e).forEach(t=>{const s=e[t];this.uniforms[t]=s.value})}createBuffers(e){this.gl;const t=this.data.buffers=e;this.buffers={},Object.keys(t).forEach(s=>{const i=t[s];i.buffer=this.createBuffer("a_"+s,i.size)})}createBuffer(e,t){const s=this.gl,i=this.program,m=s.getAttribLocation(i,e),a=s.createBuffer();return s.bindBuffer(s.ARRAY_BUFFER,a),s.enableVertexAttribArray(m),s.vertexAttribPointer(m,t,s.FLOAT,!1,0,0),a}setBuffer(e,t){const s=this.gl,i=this.data.buffers;e==null&&!s.bindBuffer(s.ARRAY_BUFFER,null)||(s.bindBuffer(s.ARRAY_BUFFER,i[e].buffer),s.bufferData(s.ARRAY_BUFFER,new Float32Array(t),s.STATIC_DRAW))}updateBuffers(){this.gl;const e=this.buffers;Object.keys(e).forEach(t=>e[t]=buffer.data),this.setBuffer(null)}createTexture(e){const t=this.gl,s=t.createTexture();t.bindTexture(t.TEXTURE_2D,s),t.texImage2D(t.TEXTURE_2D,0,t.RGBA,1,1,0,t.RGBA,t.UNSIGNED_BYTE,new Uint8Array([0,0,0,0])),this.texture=s,e&&(this.uniforms.hasTexture=1,this.loadTexture(e))}loadTexture(e){const t=this.gl,s=this.texture,i=new Image;i.onload=()=>{t.bindTexture(t.TEXTURE_2D,s),t.texImage2D(t.TEXTURE_2D,0,t.RGBA,t.RGBA,t.UNSIGNED_BYTE,i),t.texParameteri(t.TEXTURE_2D,t.TEXTURE_MIN_FILTER,t.LINEAR),t.texParameteri(t.TEXTURE_2D,t.TEXTURE_MAG_FILTER,t.LINEAR),t.texParameteri(t.TEXTURE_2D,t.TEXTURE_WRAP_S,t.CLAMP_TO_EDGE),t.texParameteri(t.TEXTURE_2D,t.TEXTURE_WRAP_T,t.CLAMP_TO_EDGE)},i.src=e}update(){const e=this.gl,t=performance.now(),s=(t-this.time.start)/5e3,i=t-this.time.old;this.time.old=t,this.uniforms.time=s,this.count>0&&(e.clear(e.COLORBUFFERBIT),e.drawArrays(e.POINTS,0,this.count)),this.onUpdate(i),requestAnimationFrame(this.update)}}return L(()=>{setTimeout(()=>{new n(document.querySelector(".waves"),{texture:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAb1BMVEUAAAD///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////8v0wLRAAAAJHRSTlMAC/goGvDhmwcExrVjWzrm29TRqqSKenRXVklANSIUE8mRkGpv+HOfAAABCElEQVQ4y4VT13LDMAwLrUHteO+R9f/fWMfO6dLaPeKVEECRxOULWsEGpS9nULDwia2Y+ALqUNbAWeg775zv+sA4/FFRMxt8U2FZFCVWjR/YrH4/H9sarclSKdPMWKzb8VsEeHB3m0shkhVCyNzeXeAQ9Xl4opEieX2QCGnwGbj6GMyjw9t1K0fK9YZunPXeAGsfJtYjwzxaBnozGGorYz0ypK2HzQSYx1y8DgSRo2ewOiyh2QWOEk1Y9OrQV0a8TiBM1a8eMHWYnRMy7CZ4t1CmyRkhSUvP3gRXyHOCLBxNoC3IJv//ZrJ/kxxUHPUB+6jJZZHrpg6GOjnqaOmzp4NDR48OLxn/H27SRQ08S0ZJAAAAAElFTkSuQmCC",uniforms:{size:{type:"float",value:2.5},field:{type:"vec3",value:[0,0,0]},speed:{type:"float",value:5}},vertex:`
+    #define M_PI 3.1415926535897932384626433832795
+
+    precision highp float;
+
+    attribute vec4 a_position;
+    attribute vec4 a_color;
+
+    uniform float u_time;
+    uniform float u_size;
+    uniform float u_speed;
+    uniform vec3 u_field;
+    uniform mat4 u_projection;
+
+    varying vec4 v_color;
+
+    void main() {
+
+      vec3 pos = a_position.xyz;
+
+      pos.y += (
+        cos(pos.x / u_field.x * M_PI * 8.0 + u_time * u_speed) +
+        sin(pos.z / u_field.z * M_PI * 8.0 + u_time * u_speed)
+      ) * u_field.y;
+
+      gl_Position = u_projection * vec4( pos.xyz, a_position.w );
+      gl_PointSize = ( u_size / gl_Position.w ) * 100.0;
+
+      v_color = a_color;
+
+    }`,fragment:`
+    precision highp float;
+
+    uniform sampler2D u_texture;
+
+    varying vec4 v_color;
+
+    void main() {
+
+      gl_FragColor = v_color * texture2D(u_texture, gl_PointCoord);
+
+    }`,onResize(e,t,s){const i=[],m=[],a=400*(e/t),f=400,h=3,T=5;for(let g=0;g<a;g+=T)for(let v=0;v<f;v+=T)i.push(-a/2+g,-30,-f/2+v),m.push(0,1-g/a*1,.5+g/a*.5,v/f);this.uniforms.field=[a,h,f],this.buffers.position=i,this.buffers.color=m,this.uniforms.size=t/400*2.5*s}})},1e3)}),(l,e)=>(p(),_("div",et,e[0]||(e[0]=[u("div",{class:"waves"},null,-1)])))}}),nt=b(st,[["__scopeId","data-v-19f0a78b"]]);var U=E({name:"Vue3Autocounter",interval:null,props:{startAmount:{type:Number,default:0},endAmount:{type:Number,default:0,required:!0},duration:{type:Number,default:3,validator(d){return d>0}},autoinit:{type:Boolean,default:!0},prefix:{type:String,default:""},suffix:{type:String,default:""},separator:{type:String,default:","},decimalSeparator:{type:String,default:"."},decimals:{type:Number,default:0,validator(d){return d>=0}}},data(){return{timestamp:0,startTimestamp:0,currentAmount:0,currentStartAmount:0,currentDuration:0,paused:!1,remaining:0,animationFrame:0}},mounted(){this.currentAmount=this.startAmount,this.currentStartAmount=this.startAmount,this.currentDuration=this.duration*1e3,this.remaining=this.duration*1e3,this.autoinit?this.start():this.paused=!0,this.$emit("mounted")},unmounted(){this.cancelAnimation()},watch:{startAmount(){this.reset()},endAmount(){this.reset()},duration(){this.reset()}},computed:{isCountingUp(){return this.endAmount>this.startAmount},displayedAmount(){return`${this.prefix}${this.formatedAmount}${this.suffix}`},formatedAmount(){const d=/(\d+)(\d{3})/;let n=this.currentAmount.toFixed(this.decimals);n+="";let l=n.split("."),e=l[0],t=l.length>1?this.decimalSeparator+l[1]:"",s=!isNaN(parseFloat(this.separator));if(this.separator&&!s)for(;d.test(e);)e=e.replace(d,"$1"+this.separator+"$2");return e+t}},methods:{start(){this.cancelAnimation(),this.currentStartAmount=this.startAmount,this.startTimestamp=null,this.currentDuration=this.duration*1e3,this.paused=!1,this.animationFrame=window.requestAnimationFrame(this.counting)},pause(){this.paused||(this.cancelAnimation(),this.paused=!0)},resume(){this.paused&&(this.startTimestamp=null,this.currentDuration=+this.remaining,this.currentStartAmount=+this.currentAmount,this.animationFrame=window.requestAnimationFrame(this.counting),this.paused=!1)},reset(){this.paused=!1,this.startTimestamp=null,this.cancelAnimation(),this.currentAmount=this.startAmount,this.autoinit?this.start():this.paused=!0},counting(d){this.timestamp=d,this.startTimestamp||(this.startTimestamp=d);let n=d-this.startTimestamp;this.remaining=this.currentDuration-n,this.isCountingUp?(this.currentAmount=this.currentStartAmount+(this.endAmount-this.currentStartAmount)*(n/this.currentDuration),this.currentAmount=this.currentAmount>this.endAmount?this.endAmount:this.currentAmount):(this.currentAmount=this.currentStartAmount-(this.currentStartAmount-this.endAmount)*(n/this.currentDuration),this.currentAmount=this.currentAmount<this.endAmount?this.endAmount:this.currentAmount),n<this.currentDuration?this.animationFrame=window.requestAnimationFrame(this.counting):this.$emit("finished")},cancelAnimation(){this.animationFrame&&window.cancelAnimationFrame(this.animationFrame)}}});function it(d,n,l,e,t,s){return p(),_("span",null,S(d.displayedAmount),1)}U.render=it;var A=(()=>{const d=U;return d.install=n=>{n.component("Vue3Autocounter",d)},d})();const rt={class:"flex home"},ot={class:"content"},at={class:"banner"},ut={class:"search"},lt={class:"flex count"},dt={class:"item"},mt={class:"number"},ct={class:"item"},ft={class:"number"},ht={class:"item"},_t={class:"number"},pt={class:"item"},gt={class:"number"},At=E({__name:"HomeView",setup(d){console.log(A);const n=R("");return(l,e)=>{const t=k,s=y,i=M;return p(),_("div",rt,[r(X),u("div",ot,[r(tt),u("div",at,[e[1]||(e[1]=u("img",{src:$,alt:""},null,-1)),e[2]||(e[2]=u("div",{class:"slog"},"让我们共同为世界创造更多的价值",-1)),r(nt,{class:"pointWave"}),e[3]||(e[3]=u("div",{class:"about"},"关于我们",-1))]),u("div",ut,[r(i,{class:"searchInput",placeholder:"快速搜索","prefix-icon":"el-icon-search",size:"large",modelValue:n.value,"onUpdate:modelValue":e[0]||(e[0]=m=>n.value=m)},{append:o(()=>[r(s,null,{default:o(()=>[r(t)]),_:1})]),_:1},8,["modelValue"])]),u("div",lt,[u("div",dt,[u("div",mt,[r(x(A),{ref:"counter",startAmount:0,endAmount:35636,duration:1,separator:",",decimalSeparator:".",decimals:0,autoinit:!0},null,512)]),e[4]||(e[4]=u("div",{class:"text"},"已完成订单",-1))]),u("div",ct,[u("div",ft,[r(x(A),{ref:"counter",startAmount:0,endAmount:19126,duration:1,separator:",",decimalSeparator:".",decimals:0,autoinit:!0},null,512)]),e[5]||(e[5]=u("div",{class:"text"},"在询订单",-1))]),u("div",ht,[u("div",_t,[r(x(A),{ref:"counter",startAmount:0,endAmount:26312,duration:1,separator:",",decimalSeparator:".",decimals:0,autoinit:!0},null,512)]),e[6]||(e[6]=u("div",{class:"text"},"服务端",-1))]),u("div",pt,[u("div",gt,[r(x(A),{ref:"counter",startAmount:0,endAmount:89126,duration:1,separator:",",decimalSeparator:".",decimals:0,autoinit:!0},null,512)]),e[7]||(e[7]=u("div",{class:"text"},"资源池",-1))])])])])}}}),xt=b(At,[["__scopeId","data-v-fd64a65b"]]);export{xt as default};
